2a24196267c752abe0a99578b4b169a74675fc9c
Arsen-melk <116881650+Arsen-melk@users.noreply.github.com>
2024-05-14 21:22:28 +0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,8 +16,8 @@ dotenv.config();
 async function connect() {
   try {
     await mongoose.connect(process.env.MONGO_URL,{
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+      useNewUrlParser:true,
+      useUnifiedTopology:true
     });
     console.log("Connected to MongoDB");
   } catch (error) {
